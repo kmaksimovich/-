641338ed0307a65190077029ba10d7360e900633
kmaksimovich <maksimovich.kirill.1999@gmail.com>
2024-11-28 20:58:10 +1100

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -5,8 +5,9 @@ my_dict['кещ'] = 2003
 print(my_dict)
 my_dict.update({'слизар' : 1997, 'шерк' : 1999, 'вова' : 1999})
 print(my_dict)
-del my_dict ['слизар']
-print(my_dict)
+delet =  my_dict.pop('слизар')
+print('удалено:', delet)
+print('модификация', my_dict)
 
 my_set = {'tron', 7, 'yula', 8}
 my_set.update({'home' , 1})
