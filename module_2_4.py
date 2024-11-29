9503c8e9952d45542550cf73421c08698fcb3e3e
kmaksimovich <maksimovich.kirill.1999@gmail.com>
2024-11-29 20:48:44 +1100

    Create module_2_4.py

---
+++ module_2_4.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i < 2:
+        continue
+    is_prime = True
+    for k in range(2, int(i ** 0.5) + 1):
+        if i % k == 0:
+            is_prime = False
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print(primes)
+print(not_primes)
